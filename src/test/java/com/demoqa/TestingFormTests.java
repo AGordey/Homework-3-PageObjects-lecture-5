ebeeadf08d0cca8beb295305b1c34140ef4ff9ef
AGordey <gordey.andrej@gmail.com>
2022-04-13 23:47:49 +0300

    HW-4 past with variables and Faker

--- src/test/java/com/demoqa/TestingFormTests.java
+++ src/test/java/com/demoqa/TestingFormTests.java
@@ -1,6 +1,7 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +13,30 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class TestingFormTests {
+    Faker faker = new Faker();
+
+    String  firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            currentAddress = faker.address().streetAddress(),
+            email = faker.internet().emailAddress(),
+            number = faker.numerify("##########"),
+            yearBD = "2000",
+            mounthBD = "January",
+            dayBD = "19",
+            gender = "Male",
+            subjects = "Hindi",
+            hobbie = "Reading",
+            stateCity = "Haryana",
+            city = "Karnal";
+
+
 
     @BeforeAll
     static void setUp() {
-        //  Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
-
     @Test
     void fillFormTest () {
         open("/automation-practice-form");
@@ -27,32 +44,32 @@ public class TestingFormTests {
         executeJavaScript("$('#fixedban').remove()");
 
         //Простые поля для заполнения
-        $("#firstName").setValue("OurFirstName");
-        $("#lastName").setValue("OurLastName");
-        $("#userEmail").setValue("OurEmail@email.com");
-        $("#userNumber").setValue("1234567890");
-        $("#currentAddress").setValue("OurCurrentAddress");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#userNumber").setValue(number);
+        $("#currentAddress").setValue(currentAddress);
 
         //Поле с радиобаттоном - выбор пола
-        $("#genterWrapper").$(byText("Male")).click();
+        $("#genterWrapper").$(byText(gender)).click();
 
         //Дата рождения
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOption("2000");
-        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption(yearBD);
+        $(".react-datepicker__month-select").selectOption(mounthBD);
         $(".react-datepicker__day.react-datepicker__day--019").click();
 
         //Поле предметов - выпадающее меню
-        $("#subjectsInput").setValue("Hindi").pressEnter();
+        $("#subjectsInput").setValue(subjects).pressEnter();
 
         //Выбор хобби
-        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText(hobbie)).click();
 
         //Выбор города
         $("#stateCity-wrapper").click();
-        $(byText("Haryana")).click();
+        $(byText(stateCity)).click();
         $("#city").click();
-        $(byText("Karnal")).click();
+        $(byText(city)).click();
 
         //Зaгрузка файла
         $("#uploadPicture").uploadFromClasspath("1.png");
@@ -62,17 +79,18 @@ public class TestingFormTests {
 
         //Проверка введенных данных
         $(".modal-body").shouldHave
-                (text("OurFirstName"),
-                        text("OurLastName"),
-                        text("OurEmail@email.com"),
-                        text("1234567890"),
-                        text("Male"),
-                        text("Hindi"),
-                        text("Reading"), text("Haryana"),
-                        text("Karnal"),
+                (text(firstName),
+                        text(lastName),
+                        text(email),
+                        text(number),
+                        text(gender),
+                        text(subjects),
+                        text(hobbie),
+                        text(stateCity),
+                        text(city),
                         text("1.png"),
-                        text("19 January,2000"),
-                        text("OurCurrentAddress"));
+                        text(dayBD+" " + mounthBD + "," + yearBD),
+                        text(currentAddress));
 
     }
 }
